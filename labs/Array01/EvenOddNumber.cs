787e7a5ac58d8ce2fad5e839ac5883433c32236e
rahulgayke1704 <rahulgayke1704@gmail.com>
2026-08-15 03:58:08 +0530

    EVEN and Odd Number

---
+++ labs/Array01/EvenOddNumber.cs
@@ -0,0 +1,18 @@
+//Count Even and Odd numbers	
+namespace EO;
+using IAMARRAY;
+
+
+public class EvenOdd
+{
+    ARRAY obj6=new ARRAY();
+
+    public int FindEvenOdd()
+    {
+        for(int i = obj6.arr.Length; i++)
+        {
+            
+        }
+    }
+    
+}
--- labs/Array01/obj/Debug/net10.0/Array01.AssemblyInfo.cs
+++ labs/Array01/obj/Debug/net10.0/Array01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa33e533a1d419548e13df7faba9b3617533591")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c2fcd2d366c73ec87507832b84e2b601647bac1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
